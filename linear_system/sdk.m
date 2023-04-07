8ca71b1c1ddfab62c681410849634f7078ec5c8c
Ralf Poeppel <ralf@poeppel-familie.de>
2023-04-07 09:22:29 +0200

    add computation of echolon form to sdkall

--- linear_system/sdk.m
+++ linear_system/sdk.m
@@ -1,77 +1,63 @@
-% Gleichungssystem Lateinisches Quadrat Ordnung n 
-% Erweiterte Koeffiziintenmatrix berechnen
+% Generate extended coefficient matrix of the
+% system of linear equations of a sudoku of order n.
 %
-% Ralf Poeppel, 2023-01-05
+% Ralf Poeppel, 2023-01-05, last update 2023-04-07
 %
-% lq erstellt die erweiterte Koeffizientenmatrix Bc eines
-% Sudokus, berechnet den Rang und die
-% Zeilenstufenform von Bc.
+% Input
+%   n: order of sudoku
+%   l: count of rows of block, must be divider of n
 %
-% Eingabeparameter
-%   n: Ordnung des Sudokus
-%   l: Zeilenzahl der Blöcke
+% Return
+%   Bc: extended coefficient matrix
 %
-% Rückgabewerte
-%   Bc: Erweiterte Koeffizientenmatrix
-%   Br: Zeilenstufenform von Bc
-%   rk: Rang der Matrix
+% Error abort
+%   n is not divisible by l
 %
-% Fehlerabbruch
-%   n ist nicht durch l teilbar
-function [Bc Br rk] = sdk(n, l)
+function Bc = sdk(n, l)
   if mod(n, l) ~= 0
-    error("Ordnung n ist nicht durch Blockzeilenzahl l teilbar");
+    error("Order n is not divisible by l");
   end
-  m = n/l; % Blockspaltenzahl
-  %fprintf("Blocksize is l = %d rows x m = %d columns\n", l, m);
+  m = n/l; % count of columns of block
   
-  c = n*(n+1)/2; % rechte Seite
-  Bc = zeros(3*n, n*n+1); % erweiterte Koeffizientenmatrix
+  c = n*(n+1)/2; % right side
+  Bc = zeros(3*n, n*n+1); % extended coefficient matrix
   
-  % Zeilen
+  % columns
   for i = 1:n
     for j = 1:n
       Bc(i, (j-1)*n+i) = 1;
     end
   end
   
-  % Spalten
+  % rows
   for i = 1:n
     for j = 1:n
       Bc(i+n, j+n*(i-1)) = 1;
     end
   end
   
-  % Blöcke
+  % blocks
   eqio = 2*n;
-  % fprintf("%d\n", eqio);
   for no = 0:m-1
     nio = eqio + no*l;
     njo = no*n*l;
-    % fprintf("  %d, %d\n", nio, njo);
     for mo = (0:l-1)
       i = nio + mo + 1;
       nmjo = njo+mo*m;
-      % fprintf("    %d, %d\n", i, nmjo);
       for lo = n*(0:l-1)
         nmljo = nmjo + lo;
-        % fprintf("      %d, %d\n", i, nmljo);
         for ja = 1:m
           j = nmljo + ja;
-          % fprintf("        %d, %d\n", i, j);
           Bc(i,j) = 1;
         end
       end
     end
-    %fprintf("\n");
   end
   
-  % rechte Seite
+  % right side
   j = n*n+1;
   for i = 1:3*n
     Bc(i, j) = c;
   end
 
-  Br = rref(Bc);
-  rk = rank(Br);
 end
--- linear_system/sdkall.m
+++ linear_system/sdkall.m
@@ -1,7 +1,8 @@
-% Berechnung der Koeffizeientenmatrix aller Sudokus 
-% der Ordnung 4, 6, 8 und 9
+% Generate extended coefficient matrix of the
+% system of linear equations of a sudoku 
+% of orders 4, 6, 8, and 9
 %
-% Ralf Poeppel, 2023-01-07
+% Ralf Poeppel, 2023-01-07, last update 2023-04-07
 %
 sdks = [4 2; 6 2; 6 3; 8 2; 8 4; 9 3];
 [nmax, cols] = size(sdks);
@@ -9,10 +10,17 @@ for i = 1:nmax
   n = sdks(i,1);
   l = sdks(i,2);
   m = n/l;
-  fprintf("\nSudoku Ordnung n = %d, Blocksize l = %d rows, m = %d columns\n", n, l, m);
+  fprintf("\nSudoku Order n = %d, Blocksize l = %d rows, m = %d columns\n", n, l, m);
   fprintf("=========================================================\n", n, l, m);
-  [Bc, Br, rk] = sdk(n,l);
+  Bc = sdk(n,l);
+  Br = rref(Bc);
+  rk = rank(Br);
   printMatrix(Bc, "Bc");
   printMatrix(Br, "Bc rred");
   fprintf("\nRank(Bc rred) = %d\n", rk);
+  [Be re] = echolonForm(Bc);
+  if re != rk
+    error("rank has changed.")
+  endif
+  printMatrix(Be, "Bc echolon form");
 end
