80cc698f50656392fe9c231eccb54181d7cc179d
Ralf Poeppel <ralf@poeppel-familie.de>
2023-04-07 09:20:13 +0200

    add computation of echolon form to lqall

--- linear_system/lq.m
+++ linear_system/lq.m
@@ -1,40 +1,36 @@
-% Gleichungssystem Lateinisches Quadrat Ordnung n 
-% Erweiterte Koeffizeintenmatrix berechnen
+% Generate extended coefficient matrix of the
+% system of linear equations of a latin square of order n.
 %
-% Ralf Poeppel, 2023-01-05
+% Ralf Poeppel, 2023-01-05, last update 2023-04-07
 %
-% lq erstellt die erweiterte Koeffizientenmatrix Bc eines
-% lateinischen Quadrates, berechnet den Rang und die
-% Zeilenstufenform von Bc.
+% Input
+%   n: order of latin square
 %
-% Eingabeparameter
-%   n: Ordnung des Lateinischen Quadrats
-%
-% Rückgabewerte
-%   Bc: Erweiterte Koeffizientenmatrix
-%   Br: Zeilenstufenform von Bc
-%   rk: Rang der Matrix
+% Return
+%   Bc: extended coefficient matrix
 %
 function [Bc Br rk] = lq(n)
   c = n*(n+1)/2;
   Bc = zeros(2*n, n*n+1);
-  Br = Bc;
-  rk = 0;
+
+  % columns
   for i = 1:n
     for j = 1:n
       Bc(i, (j-1)*n+i) = 1;
     end
   end
+
+  % rows
   for i = 1:n
     for j = 1:n
       Bc(i+n, j+n*(i-1)) = 1;
     end
   end
+
+  % right side
   j = n*n+1;
   for i = 1:2*n
     Bc(i, j) = c;
   end
-  %rk = rank(Bc);
-  Br = rref(Bc);
-  rk = rank(Br);
+
 end
--- linear_system/lqall.m
+++ linear_system/lqall.m
@@ -1,13 +1,21 @@
-% Berechnung der Koeffizeientenmatrix aller lateinischen Quadrate 
-% der Ordnung 1 bis 9
+% Generate extended coefficient matrix of the
+% system of linear equations of a latin square
+% of orders 1 to 9
 %
-% Ralf Poeppel, 2023-01-11
+% Ralf Poeppel, 2023-01-11, last update 2023-04-07
 %
 for n = 1:9
-  fprintf("\nLateinisches Quadrat der Ordnung n = %d\n", n);
+  fprintf("\nLatin square of order n = %d\n", n);
   fprintf("======================================\n");
-  [Bc Br rk] = lq(n);
+  Bc = lq(n);
+  Br = rref(Bc);
+  rk = rank(Br);
   printMatrix(Bc, "Bc");
   printMatrix(Br, "Bc rred");
   fprintf("\nRank(Bc rred) = %d\n", rk);
+  [Be re] = echolonForm(Bc);
+  if re != rk
+    error("rank has changed.")
+  endif
+  printMatrix(Be, "Bc echolon form");
 end
